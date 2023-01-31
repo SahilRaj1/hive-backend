d3651de810fbbd23f947c833bd5e4fa43a3498bd
SahilRaj1 <raj24sahil@gmail.com>
2023-01-31 19:52:23 +0530

    all post routes done except POST /posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,10 +1,42 @@
 const Post = require(`${__dirname}/../models/PostModel`);
 
-// GET /posts : Retrieve a list of all posts
+// GET /posts : Retrieve a list of all posts (Login Required)
 exports.fetchAllPosts = async (req, res) => {
     try {
+
         const posts = await Post.find();
-        res.json(posts);
+
+        // sending response
+        res.status(200).json({
+            status: 'success',
+            results: posts.length,
+            data: {
+              posts,
+            },
+          });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+};
+
+// GET /posts/:user_id : Retrieve a list of all posts by a specific user (Login Required)
+exports.fetchAllPostsOfUser = async (req, res) => {
+    try {
+
+        // finding posts by the specified user
+        const posts = await Post.find({user_id: req.params.user_id});
+
+        // sending response
+        res.status(200).json({
+            status: 'success',
+            results: posts.length,
+            data: {
+              posts,
+            },
+          });
+
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Internal server error");
@@ -14,11 +46,95 @@ exports.fetchAllPosts = async (req, res) => {
 // GET /posts/:id : Retrieve a specific post by id (Login Required)
 exports.fetchPost = async (req, res) => {
     try {
+
+        // finding the post to be fetched
         const post = await Post.findById(req.params.id);
         if (!post) {
             return res.status(404).send("Not Found");
         }
-        res.json({ post });
+
+        // sending response
+        res.status(200).json({
+            status: 'success',
+            results: 1,
+            data: {
+              post,
+            },
+          });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+};
+
+// PUT /posts/:id : Update a specific post by id (Login Required)
+exports.updatePost = async (req, res) => {
+    try {
+
+        const { caption } = req.body;
+        const newPost = {};
+
+        if (caption) {
+            newPost.caption = caption;
+            newPost.updated_on = Date.now();
+        }
+
+        // finding the post to be updated
+        let post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).send("Not found");
+        }
+
+        // checking if a user is updating someone else's post
+        if (post.user_id.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed");
+        }
+
+        // updating the post
+        post = await Post.findByIdAndUpdate(
+            req.params.id,
+            { $set: newPost },
+            { new: true }
+        );
+
+        // sending response
+        res.status(202).json({
+            status: 'success',
+            results: 1,
+            data: {
+              post,
+            },
+          });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+};
+
+// DELETE /posts/:id : Delete a specific post by id (Login Required)
+exports.deletePost = async (req, res) => {
+    try {
+
+        // finding the post to be deleted
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).send("Not found");
+        }
+
+        // deleting the post
+        post = Post.findByIdAndDelete(req.params.id);
+
+        // sending response
+        res.status(200).json({
+            status: 'success',
+            results: 1,
+            data: {
+              post,
+            },
+          });
+
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Internal server error");
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const fetchUser = require(`${__dirname}/../middlewares/fetchUser`);
 const postController = require(`${__dirname}/../controllers/postController`);
 
-router.get("/", postController.fetchAllPosts);
+router.get("/", fetchUser, postController.fetchAllPosts);
+router.get("/:user_id", fetchUser, postController.fetchAllPostsOfUser);
 router.get("/:id", fetchUser, postController.fetchPost);
+router.put("/:id", fetchUser, postController.updatePost);
+router.delete("/:id", fetchUser, postController.deletePost);
 
 module.exports = router;
