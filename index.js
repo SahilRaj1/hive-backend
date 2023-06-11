ffd4cd1fbcb7d462cbf384a4309dabc1bd97c04e
SahilRaj1 <raj24sahil@gmail.com>
2023-06-12 03:00:02 +0530

    tried automated documentation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const connectToMongo = require('./db');
-const express = require('express');
-const cors = require('cors');
-const env = require('dotenv');
-const path = require('path')
-env.config({ path: './.env' });
+const connectToMongo = require("./db");
+const express = require("express");
+const cors = require("cors");
+const gulp = require('gulp');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const env = require("dotenv");
+const path = require("path");
+env.config({ path: "./.env" });
 
 // AVAILABLE ROUTERS
 const authRouter = require(`${__dirname}/routes/authRoutes`);
@@ -20,11 +24,31 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// AVAIABLE ROUTES
-app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter, followRouter);
-app.use('/api/posts', postRouter, likeRouter, commentRouter);
+// Swagger configuration options
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Hive API',
+            version: '1.0.0',
+        },
+    },
+    apis: [`${__dirname}/swagger.json`],
+};
 
-app.listen(port, ()=> {
+gulp.task('generate-docs', () => {
+    const specs = swaggerJsdoc(options);
+    fs.writeFileSync(`${__dirname}/swagger.json`, JSON.stringify(specs, null, 2));
+});
+
+// Serve Swagger API documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(`${__dirname}/swagger.json`));
+
+// AVAIABLE ROUTES
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter, followRouter);
+app.use("/api/posts", postRouter, likeRouter, commentRouter);
+
+app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
 });
